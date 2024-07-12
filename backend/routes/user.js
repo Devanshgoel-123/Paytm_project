ae9963a6d4fed6e99e4c1a574e213b489bb94c83
Devansh <f20220457@pilani.bits-pilani.ac.in>
2024-07-13 00:32:05 +0530

    made the user authentication using jwt

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,37 @@
 const express=require("express");
-
+const {z} =require("zod");
+const {User}=require("../db");
+const jwt=require("jsonwebtoken");
 const router=express.Router();
+const {JWT_SECRET}=require("../config")
 
+const userSchema=z.object({
+    username:z.string().email(),
+    firstname:z.string(),
+    password:z.string(),
+    lastname:z.string()
+})
+
+router.post("/signup",async (req,res)=>{
+    const body=req.body;
+    const {success}=userSchema.safeParse(body);
+    if(!success){
+        return res.status(411).json({
+            message:"Incorrect Inputs"
+        })
+    }
+    const user=User.findOne({username:body.username});
+    if(user._id){
+        return res.json({
+            message:"Username already taken"
+        })
+    }
+    const dbUser=await User.create(body);
+    const token=jwt.sign({userId:user._id},JWT_SECRET);
+    res.status(200).json({
+        message:"User created Successfully",
+        token:token,
+    })
+    
+})
 module.exports=router;
